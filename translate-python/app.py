282ed8019216ab258fcbfd8fd7d51a58517181a3
mmartin4972 <mmartin4972@gmail.com>
2022-10-16 16:24:18 -0400

    Renamed some stuff

--- translate-python/app.py
+++
@@ -1,8 +0,0 @@
-from googletrans import Translator
-
-translator = Translator()
-result = translator.translate('Mikä on nimesi', src='fi', dest='fr')
-
-print(result.src)
-print(result.dest)
-print(result.text)
