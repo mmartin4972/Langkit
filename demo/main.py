679d762410d6012f5bf8f82a81040d2f846e6e75
Carson Ellis <crsnell25@gmail.com>
2022-10-17 14:13:34 -0400

    GUI built

--- demo/main.py
+++ demo/main.py
@@ -9,6 +9,12 @@ eel.init(f'{path.dirname(path.realpath(__file__))}/templates')
 def random_python():
     print("Random function running")
     return randint(1,100)
+
+
+@eel.expose
+def mic_click():
+    print("Mic clicked (python console)")
+
   
 # Start the index.html file
-eel.start("index.html", port=8080)
+eel.start("index.html", size=(1000, 665), port=8080)
