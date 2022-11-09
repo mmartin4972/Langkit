a894907ac809b4002ddd1318dc1c9948acfed881
mmartin4972 <mmartin4972@gmail.com>
2022-11-09 11:09:38 -0500

    Google credential hack

--- server/wsgi.py
+++ server/wsgi.py
@@ -1,4 +1,10 @@
 from main import app as application
+import os
 
-if __name__ == "__main__":  
+if __name__ == "__main__":
+    contents = os.getenv('GOOGLE_CREDENTIALS')
+    f = open("/app/cred.json", "w")
+    f.write(contents)
+    f.close()
+
     application.run()
