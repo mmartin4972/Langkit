278354d72c208d8a2ac4ee18cd0df6b3ba7c389c
mmartin4972 <mmartin4972@gmail.com>
2022-11-29 13:58:01 -0500

    Modified code to use newer GPT3 model

--- server/server_tests.py
+++ server/server_tests.py
@@ -16,26 +16,26 @@ headers = {'Content-Type': 'application/json', 'Accept':'application/json'}
 res = requests.post(url + "/parse-cmd", json=data)
 print(res.json())
 
-# data1 = [
-#     {"from": "en", "to": "es", "text": "the school is on fire"}
-# ]
-# res1 = requests.post(url + "/translate", json=data1)
-# print(res1.json())
+data1 = [
+    {"from": "en", "to": "es", "text": "the school is on fire"}
+]
+res1 = requests.post(url + "/translate", json=data1)
+print(res1.json())
 
-# data2 = [
-#     {"prompt": "Generate one word about winter"}
-# ]
-# res2 = requests.post(url + "/naive-gpt3-res", json=data2)
-# print(res2.json())
+data2 = [
+    {"prompt": "Generate one word about winter"}
+]
+res2 = requests.post(url + "/naive-gpt3-res", json=data2)
+print(res2.json())
 
-# data3 = [{'cmd':'generate me phrases about orange birds', 'to':'es', 'from':'en'}]
-# res3 = requests.post(url + "/process", json=data3)
-# print(res3.json())
+data3 = [{'cmd':'generate me phrases about orange birds', 'to':'es', 'from':'en'}]
+res3 = requests.post(url + "/process", json=data3)
+print(res3.json())
 
-# data4 = [{'cmd':'generate me words about orange birds', 'to':'es', 'from':'en'}]
-# res4 = requests.post(url + "/process", json=data4)
-# print(res4.json())
+data4 = [{'cmd':'generate me words about orange birds', 'to':'es', 'from':'en'}]
+res4 = requests.post(url + "/process", json=data4)
+print(res4.json())
 
-# data5 = [{'cmd':'translate the word orange', 'to':'es', 'from':'en'}]
-# res5 = requests.post(url + "/process", json=data5)
-# print(res5.json())
+data5 = [{'cmd':'translate the word orange', 'to':'es', 'from':'en'}]
+res5 = requests.post(url + "/process", json=data5)
+print(res5.json())
